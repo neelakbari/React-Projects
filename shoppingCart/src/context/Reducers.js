d04886aa65b139fcd337d308a5b6faa07a41cf1d
Neel Akbari <neel.akbari@artoon.in>
2023-06-09 16:43:47 +0530

    home component done

--- shoppingCart/src/context/Reducers.js
+++ shoppingCart/src/context/Reducers.js
@@ -1,9 +1,18 @@
+import { products } from "../data";
+
 const cartReducer = (state, action) => {
   switch (action.type) {
-    case "value":
-      return state;
+    case "ADD_TO_CART": {
+      return { ...state, cart: [...state.cart, { ...action.payload, qty: 1 }] };
+    }
+    case "REMOVE_FROM_CART":
+      return {
+        ...state,
+        cart: state.cart.filter((c) => c.id !== action.payload.id),
+      };
     default:
       return state;
   }
 };
+
 export { cartReducer };
