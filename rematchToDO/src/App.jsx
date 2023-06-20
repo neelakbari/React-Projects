55ab35e670b4c30e642801a481131f4b142ed4a6
Neel Akbari <neel.akbari@artoon.in>
2023-06-20 16:16:41 +0530

    rematch ToDo completed

--- rematchToDO/src/App.jsx
+++ rematchToDO/src/App.jsx
@@ -1,32 +1,34 @@
-import AddTaskForm from './Components/AddTaskForm';
-import {useState } from "react"
-import React from 'react';
-import ToDo from './Components/ToDo';
-import { useSelector } from 'react-redux';
-import UpdateForm from './Components/UpdateForm';
+import AddTaskForm from "./Components/AddTaskForm";
+import { useState } from "react";
+import React from "react";
+import ToDo from "./Components/ToDo";
+import { useSelector } from "react-redux";
+import UpdateForm from "./Components/UpdateForm";
 
 function App() {
-  const todo = useSelector((state) =>  state.todos.list);
+  const todo = useSelector((state) => state.todos.list);
 
-  const [newTask, setNewTask] = useState("")
-  const [updateData, setUpdateData] = useState("")
+  const [newTask, setNewTask] = useState("");
+  const [updateData, setUpdateData] = useState("");
   return (
     <div className="App container">
-
       <h2>ToDo List</h2>
-      {
-        updateData  ?
-          <UpdateForm
-            setUpdateData={setUpdateData}
-            updateData ={updateData} /> :
-          <AddTaskForm
-            newTask={newTask}
-            setNewTask={setNewTask}
-          />
-      }
+      {updateData ? (
+        <UpdateForm
+          setUpdateData={setUpdateData}
+          updateData={updateData}
+        />
+      ) : (
+        <AddTaskForm
+          newTask={newTask}
+          setNewTask={setNewTask}
+        />
+      )}
       <br />
-      <ToDo todo={todo} setUpdateData={setUpdateData} />
-
+      <ToDo
+        todo={todo}
+        setUpdateData={setUpdateData}
+      />
     </div>
   );
 }
